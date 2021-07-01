ee428922c6fab73d4b7410cd314634c14b605bfa
Matej Hospodar <matejhospodar123@gmail.com>
2021-07-01 17:51:50 +0200

    recapcha disabled

--- src/middlewares/recaptchaMiddleware.ts
+++ src/middlewares/recaptchaMiddleware.ts
@@ -8,26 +8,26 @@ const googleServiceConfig: IGoogleServiceConfig = config.get('googleService')
 
 export default async (req: Request, _res: Response, next: NextFunction) => {
 	try {
-		const recaptchaResponse = req.body.recaptcha
+		// const recaptchaResponse = req.body.recaptcha
 
-		const response = await axios({
-			method: 'POST',
-			url: 'https://recaptchaenterprise.googleapis.com/v1beta1/projects/kupaliska/assessments?key=AIzaSyBylFCQ0PBYSTjdwxOjtIvA6K7Cv7xBZjg',
-			headers: { 'Content-Type': 'application/json' },
-			data: {
-				event: {
-					siteKey: googleServiceConfig.recaptcha.clientSecret,
-					token: recaptchaResponse,
-					expectedAction: "order"
-				}
-			}
-		  })
+		// const response = await axios({
+		// 	method: 'POST',
+		// 	url: 'https://recaptchaenterprise.googleapis.com/v1beta1/projects/kupaliska/assessments?key=AIzaSyBylFCQ0PBYSTjdwxOjtIvA6K7Cv7xBZjg',
+		// 	headers: { 'Content-Type': 'application/json' },
+		// 	data: {
+		// 		event: {
+		// 			siteKey: googleServiceConfig.recaptcha.clientSecret,
+		// 			token: recaptchaResponse,
+		// 			expectedAction: "order"
+		// 		}
+		// 	}
+		//   })
 
-		if (response.status !== 200 || response.data.score < 0.5) {
-			console.log(response.data.tokenProperties.invalidReason)
-			console.log(response.data.reasons)
-			throw new ErrorBuilder(400, req.t('error:invalidRecaptcha'))
-		}
+		// if (response.status !== 200 || response.data.score < 0.5) {
+		// 	console.log(response.data.tokenProperties.invalidReason)
+		// 	console.log(response.data.reasons)
+		// 	throw new ErrorBuilder(400, req.t('error:invalidRecaptcha'))
+		// }
 		return next()
 	} catch (err) {
 		return next(err)
