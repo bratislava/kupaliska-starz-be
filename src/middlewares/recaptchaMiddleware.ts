b53b5bd777693b8271171bd57eedc86209dab57c
Matej Hospodar <matejhospodar123@gmail.com>
2021-07-07 13:38:56 +0200

    recaptcha disabled

--- src/middlewares/recaptchaMiddleware.ts
+++ src/middlewares/recaptchaMiddleware.ts
@@ -1,31 +1,31 @@
-import { Request, Response, NextFunction } from 'express'
-import config from 'config'
-import { IGoogleServiceConfig } from '../types/interfaces'
-import ErrorBuilder from '../utils/ErrorBuilder'
-import axios from 'axios'
-import formUrlEncoded from 'form-urlencoded'
+import { Request, Response, NextFunction } from "express";
+import config from "config";
+import axios from "axios";
+import formUrlEncoded from "form-urlencoded";
+import { IGoogleServiceConfig } from "../types/interfaces";
+import ErrorBuilder from "../utils/ErrorBuilder";
 
-const googleServiceConfig: IGoogleServiceConfig = config.get('googleService')
+const googleServiceConfig: IGoogleServiceConfig = config.get("googleService");
 
 export default async (req: Request, _res: Response, next: NextFunction) => {
 	try {
-		const recaptchaResponse = req.body.recaptcha
+		// const recaptchaResponse = req.body.recaptcha
 
-		const response = await axios({
-			method: 'POST',
-			url: 'https://www.google.com/recaptcha/api/siteverify',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			data: formUrlEncoded({
-				secret: googleServiceConfig.recaptcha.clientSecret,
-				response: recaptchaResponse
-			})
-		  })
+		// const response = await axios({
+		// 	method: 'POST',
+		// 	url: 'https://www.google.com/recaptcha/api/siteverify',
+		// 	headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		// 	data: formUrlEncoded({
+		// 		secret: googleServiceConfig.recaptcha.clientSecret,
+		// 		response: recaptchaResponse
+		// 	})
+		//   })
 
-		if (response.status !== 200 || response.data.success !== true) {
-			throw new ErrorBuilder(400, req.t('error:invalidRecaptcha'))
-		}
-		return next()
+		// if (response.status !== 200 || response.data.success !== true) {
+		// 	throw new ErrorBuilder(400, req.t('error:invalidRecaptcha'))
+		// }
+		return next();
 	} catch (err) {
-		return next(err)
+		return next(err);
 	}
-}
+};
