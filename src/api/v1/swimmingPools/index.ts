c5d75557565870b715aeaadd3034a25960b441e9
Adam Grund <adam.grund@gmail.com>
2022-05-03 14:03:52 +0200

    registration first contact attempt

--- src/api/v1/swimmingPools/index.ts
+++ src/api/v1/swimmingPools/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import schemaMiddleware from '../../../middlewares/schemaMiddleware'
 
+import * as GetSwimmingPoolso from './get.swimmingPoolso'
 import * as GetSwimmingPools from './get.swimmingPools'
 import * as GetSwimmingPool from './get.swimmingPool'
 
@@ -11,8 +12,10 @@ router.get('/',
 	schemaMiddleware(GetSwimmingPools.schema),
 	GetSwimmingPools.workflow)
 
+router.get('/register',
+	schemaMiddleware(GetSwimmingPoolso.schema),
+	GetSwimmingPoolso.workflow)
+
 router.get('/:swimmingPoolId',
 	schemaMiddleware(GetSwimmingPool.schema),
 	GetSwimmingPool.workflow)
-
-
