7e6751ca263dadd23e58f2fb1df0301db222203b
Adam Grund <adam.grund@gmail.com>
2022-05-25 09:47:23 +0200

    WIP SwimmingLoggedUsers

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 import OrderRouter from './orders'
 import UsersRouter from './users'
 import SwimmingPoolsRouter from './swimmingPools'
+import SwimmingLoggedUsersRouter from './swimmingLoggedUsers'
 import TicketTypesRouter from './ticketTypes'
 import ContactRouter from './contact'
 
@@ -12,6 +13,7 @@ export default () => {
 	router.use('/orders', OrderRouter())
 	router.use('/users', UsersRouter())
 	router.use('/swimmingPools', SwimmingPoolsRouter())
+	router.use('/swimmingLoggedUsers', SwimmingLoggedUsersRouter())
 	router.use('/ticketTypes', TicketTypesRouter())
 	router.use('/contact', ContactRouter())
 
