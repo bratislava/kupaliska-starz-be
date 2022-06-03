773a721f4a3bb3dcc56b70f7304330bf3a3e22fe
lukaspolesnak <lukas.polesnak@gmail.com>
2022-06-03 17:26:04 +0200

    tickets update

---
+++ src/db/migrations/0039-addLoggedUserIdTotickets.ts
@@ -0,0 +1,60 @@
+import { QueryInterface, DataTypes } from 'sequelize'
+import { checkTableExists } from '../../utils/helpers'
+export async function up(queryInterface: QueryInterface) {
+	try {
+		const exists = await checkTableExists(queryInterface, 'tickets')
+
+		if (!exists) {
+			return Promise.resolve()
+		}
+
+		const table: any = await queryInterface.describeTable('tickets')
+
+		if (!table.loggedUserId) {
+			await queryInterface.addColumn(
+				'tickets',
+				'loggedUserId',
+				{
+					type: DataTypes.UUID,
+					allowNull: true,
+				}
+			)
+		}
+
+		if (!table.associatedSwimmerId) {
+			await queryInterface.addColumn(
+				'tickets',
+				'associatedSwimmerId',
+				{
+					type: DataTypes.UUID,
+					allowNull: true,
+				}
+			)
+		}
+
+		return Promise.resolve()
+
+	} catch (err) {
+		throw err;
+	}
+}
+
+export async function down(queryInterface: QueryInterface) {
+	const table: any = await queryInterface.describeTable('tickets')
+
+	if (table.remainingEntries) {
+		await queryInterface.removeColumn(
+			'tickets',
+			'loggedUserId'
+		)
+	}
+
+	if (table.associatedSwimmerId) {
+		await queryInterface.removeColumn(
+			'tickets',
+			'associatedSwimmerId',
+		)
+	}
+
+	return Promise.resolve()
+}
---
+++ src/db/migrations/0040-updateAssociatedSwimmersName.ts
@@ -0,0 +1,60 @@
+import { QueryInterface, DataTypes } from 'sequelize'
+import { checkTableExists } from '../../utils/helpers'
+export async function up(queryInterface: QueryInterface) {
+	try {
+		const exists = await checkTableExists(queryInterface, 'associatedSwimmers')
+
+		if (!exists) {
+			return Promise.resolve()
+		}
+
+		const table: any = await queryInterface.describeTable('associatedSwimmers')
+
+		if (table.surname) {
+			await queryInterface.removeColumn(
+				'associatedSwimmers',
+				'surname'
+			)
+		}
+
+		if (!table.firstname) {
+			await queryInterface.addColumn(
+				'associatedSwimmers',
+				'firstname',
+				{
+					type: DataTypes.UUID,
+					allowNull: true,
+				}
+			)
+		}
+
+		return Promise.resolve()
+
+	} catch (err) {
+		throw err;
+	}
+}
+
+export async function down(queryInterface: QueryInterface) {
+	const table: any = await queryInterface.describeTable('associatedSwimmers')
+
+	if (!table.surname) {
+		await queryInterface.addColumn(
+			'associatedSwimmers',
+			'surname',
+			{
+				type: DataTypes.UUID,
+				allowNull: true,
+			}
+		)
+	}
+
+	if (table.firstname) {
+		await queryInterface.removeColumn(
+			'associatedSwimmers',
+			'firstname'
+		)
+	}
+
+	return Promise.resolve()
+}
