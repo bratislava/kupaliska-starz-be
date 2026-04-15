195201af2d727465631752cb2474d6432d5a9de9
Adam Grund <adam.grund@gmail.com>
2026-04-15 17:09:57 +0200

    add orderId in DiscountCode model in migration 57

---
+++ src/db/migrations/0057-discountCodeOrderId.ts
@@ -0,0 +1,58 @@
+import { DataTypes, QueryInterface } from 'sequelize'
+import DB from '../models'
+
+export async function up(queryInterface: QueryInterface) {
+	const transaction = await DB.transaction()
+	try {
+		const exists = await queryInterface.tableExists('discountCodes', {
+			transaction,
+		})
+
+		if (!exists) {
+			await transaction.rollback()
+			return
+		}
+
+		await queryInterface.addColumn(
+			'discountCodes',
+			'orderId',
+			{
+				type: DataTypes.UUID,
+				references: {
+					model: 'orders',
+					key: 'id',
+				},
+				allowNull: true,
+			},
+			{ transaction }
+		)
+
+		await transaction.commit()
+	} catch (err) {
+		await transaction.rollback()
+		throw err
+	}
+}
+
+export async function down(queryInterface: QueryInterface) {
+	const transaction = await DB.transaction()
+	try {
+		const exists = await queryInterface.tableExists('discountCodes', {
+			transaction,
+		})
+
+		if (!exists) {
+			await transaction.rollback()
+			return
+		}
+
+		await queryInterface.removeColumn('discountCodes', 'orderId', {
+			transaction,
+		})
+
+		await transaction.commit()
+	} catch (err) {
+		await transaction.rollback()
+		throw err
+	}
+}
