03be1cbfe792ab948614287fe2f06c160f5d8206
Adam Grund <adam.grund@gmail.com>
2026-04-21 16:08:41 +0200

    add missing ticketType seeder

--- src/db/seeders/test/01-ticketTypes.ts
+++ src/db/seeders/test/01-ticketTypes.ts
@@ -4,6 +4,8 @@ import { QueryInterface } from 'sequelize'
 export const ticketId = 'c70954c7-970d-4f1a-acf4-12b91acabe01'
 export const ticketTypeSeasonalWithChildren =
 	'c70954c7-970d-4f1a-acf4-12b91acabe04'
+export const ticketTypeSeasonalWithChildren2 =
+	'c70954c7-970d-4f1a-acf4-12b91acabe08'
 export const ticketTypeExpired = 'c70954c7-970d-4f1a-acf4-12b91acabe05'
 export const ticketTypeSeasonNameRequired =
 	'c70954c7-970d-4f1a-acf4-12b91acabe02'
@@ -49,6 +51,29 @@ export async function up(queryInterface: QueryInterface) {
 		{
 			id: ticketTypeSeasonalWithChildren,
 			name: 'Sezónny tiket',
+			description: faker.lorem.paragraph(15),
+			priceWithVat: 2000,
+			vatPercentage: 23,
+			type: 'SEASONAL',
+			nameRequired: false,
+			photoRequired: false,
+			childrenAllowed: true,
+			childrenMaxNumber: 2,
+			childrenAgeFrom: 3,
+			childrenAgeTo: 17,
+			childrenPriceWithVat: 100,
+			childrenVatPercentage: 23,
+			childrenAgeToWithAdult: 10,
+			childrenPhotoRequired: false,
+			validFrom: '2021-04-12',
+			validTo: '2025-07-12',
+			hasTicketDuration: false,
+			hasEntranceConstraints: false,
+		},
+		// SEASONAL WITH CHILDREN
+		{
+			id: ticketTypeSeasonalWithChildren2,
+			name: 'Sezónny tiket 2',
 			description: faker.lorem.paragraph(15),
 			priceWithVat: 2000,
 			vatPercentage: 23,
