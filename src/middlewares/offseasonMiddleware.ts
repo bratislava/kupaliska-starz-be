298abb8f78f72851987684cbc161e4e083f80201
Adam Grund <adam.grund@gmail.com>
2026-06-04 11:04:04 +0200

    change offseasonMiddleware to handle depending on generalSettings - isOffSeason variable

--- src/middlewares/offseasonMiddleware.ts
+++ src/middlewares/offseasonMiddleware.ts
@@ -1,10 +1,18 @@
 import { Request, Response, NextFunction } from 'express'
 import ErrorBuilder from '../utils/ErrorBuilder'
-import Turnstile, { TurnstileOptions, TurnstileResponse } from 'cf-turnstile'
-import { logger } from '../utils/logger'
+import { models } from '../db/models'
 
 export default async (req: Request, _res: Response, next: NextFunction) => {
-	if (process.env.OFFSEASON === 'true')
+	const { GeneralSettings } = models
+
+	const general = await GeneralSettings.findOne({
+		attributes: ['alertText', 'showAlert', 'isOffSeason'],
+	})
+
+	if (!general) {
+		throw new ErrorBuilder(404, req.t('error:generalNotFound'))
+	}
+	if (general.isOffSeason)
 		return next(new ErrorBuilder(400, req.t('error:offseason')))
 	next()
 }
