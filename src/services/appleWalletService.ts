a3d87d5ab2cf64ede2e7e18643f94216c9425c4c
Adam Grund <adam.grund@gmail.com>
2026-06-09 15:54:43 +0200

    add TODOs to `appleWalletService` and `googlePayService`

--- src/services/appleWalletService.ts
+++ src/services/appleWalletService.ts
@@ -118,7 +118,7 @@ export const createPass = async (ticket: TicketModel) => {
 			ownerName ? ` ${ownerName}` : ''
 		}`,
 		serialNumber: ticketId,
-		// this could be included in general info at start of the season
+		// TODO this could be included in general info at start of the season or from ticketType.validTo
 		expirationDate: `${year}-09-31T10:00-05:00`,
 		generic: {
 			headerFields: [
