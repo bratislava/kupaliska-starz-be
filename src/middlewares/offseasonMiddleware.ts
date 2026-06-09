4dc0e7c69de264f4c782101b164c4808119d614a
Adam Grund <adam.grund@gmail.com>
2026-06-09 15:09:04 +0200

    catch in case of missing generalSettings data

--- src/middlewares/offseasonMiddleware.ts
+++ src/middlewares/offseasonMiddleware.ts
@@ -3,17 +3,21 @@ import ErrorBuilder from '../utils/ErrorBuilder'
 import { models } from '../db/models'
 
 export default async (req: Request, _res: Response, next: NextFunction) => {
-	const { GeneralSettings } = models
+	try {
+		const { GeneralSettings } = models
 
-	const general = await GeneralSettings.findOne({
-		attributes: ['alertText', 'showAlert', 'isOffSeason'],
-	})
+		const general = await GeneralSettings.findOne({
+			attributes: ['alertText', 'showAlert', 'isOffSeason'],
+		})
 
-	// TODO: make sure that we have this info or send some default value maybe?
-	if (!general) {
-		throw new ErrorBuilder(404, req.t('error:generalNotFound'))
+		// TODO: make sure that we have this info or send some default value maybe?
+		if (!general) {
+			throw new ErrorBuilder(404, req.t('error:generalNotFound'))
+		}
+		if (general.isOffSeason)
+			return next(new ErrorBuilder(400, req.t('error:offseason')))
+		next()
+	} catch (error) {
+		return next(error)
 	}
-	if (general.isOffSeason)
-		return next(new ErrorBuilder(400, req.t('error:offseason')))
-	next()
 }
