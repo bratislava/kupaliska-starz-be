34b69f3651f4f060b618e6c91cf032d5bc009632
Adam Grund <adam.grund@gmail.com>
2026-06-11 14:37:13 +0200

    handle errors of offseasonMiddleware

--- src/middlewares/offseasonMiddleware.ts
+++ src/middlewares/offseasonMiddleware.ts
@@ -1,21 +1,26 @@
 import { Request, Response, NextFunction } from 'express'
 import ErrorBuilder from '../utils/ErrorBuilder'
 import { models } from '../db/models'
+import logger from '../utils/logger'
 
 export default async (req: Request, _res: Response, next: NextFunction) => {
 	try {
 		const { GeneralSettings } = models
 
 		const general = await GeneralSettings.findOne({
-			attributes: ['alertText', 'showAlert', 'isOffSeason'],
+			attributes: ['isOffSeason'],
 		})
 
-		// TODO: make sure that we have this info or send some default value maybe?
-		if (!general) {
-			throw new ErrorBuilder(404, req.t('error:generalNotFound'))
-		}
-		if (general.isOffSeason)
+		if (!general || general?.isOffSeason === undefined) {
+			logger.error(
+				`GeneralSettings not found. general: ${JSON.stringify(general)}`
+			)
 			return next(new ErrorBuilder(400, req.t('error:offseason')))
+		}
+		if (general.isOffSeason) {
+			return next(new ErrorBuilder(400, req.t('error:offseason')))
+		}
+
 		next()
 	} catch (error) {
 		return next(error)
