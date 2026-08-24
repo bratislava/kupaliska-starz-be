1584bf1a8449ed3b3bf34151cc10f4c9572d321e
Adam Grund <adam.grund@gmail.com>
2026-08-24 10:49:35 +0200

    2 remove automatic increase of hash used in salt generation (#262)
    remove automatic increase of hash used in salt generation

--- src/utils/authorization.ts
+++ src/utils/authorization.ts
@@ -7,19 +7,10 @@ import { IPassportConfig } from '../types/interfaces'
 const passportConfig: IPassportConfig = config.get('passport')
 
 const BCRYPT_WORK_FACTOR_BASE = 12
-const BCRYPT_DATE_BASE = 1483228800000
-const BCRYPT_WORK_INCREASE_INTERVAL = 47300000000
 
-export const hashPassword = (password: string, factor?: number) => {
+export const hashPassword = (password: string) => {
 	try {
-		const BCRYPT_CURRENT_DATE = new Date().getTime()
-		const BCRYPT_WORK_INCREASE = Math.max(
-			0,
-			Math.floor((BCRYPT_CURRENT_DATE - BCRYPT_DATE_BASE) / BCRYPT_WORK_INCREASE_INTERVAL)
-		)
-		const BCRYPT_WORK_FACTOR = Math.min(19, BCRYPT_WORK_FACTOR_BASE + BCRYPT_WORK_INCREASE)
-
-		const salt = bcrypt.genSaltSync(factor ? factor : BCRYPT_WORK_FACTOR)
+		const salt = bcrypt.genSaltSync(BCRYPT_WORK_FACTOR_BASE)
 		return bcrypt.hashSync(password, salt)
 	} catch (e) {
 		return e
